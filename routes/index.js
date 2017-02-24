08c0bb3ae8df3feb097b5d5aaa89c5c931cfe401
sseen <ssnfee@gmail.com>
2017-02-24 17:56:51 +0800

    first get dom from the website

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var request = require('request');
+var cheerio = require('cheerio');
 
 
 /* GET home page. */
@@ -16,15 +17,18 @@ router.get('/test', function(req, res, next) {
 /* POST to Add User Service */
 router.post('/test', function(req, res) {
 
-    request('http://www.bing.com', function (error, response, body) {
+    request('http://www.hainnu.edu.cn/html/xiaoyuan/', function (error, response, body) {
         if (!error && response.statusCode == 200) {
             console.log(body) // 打印google首页
+
+            var $ = cheerio.load(body);
+            var content = $("div.hnnuxysh_newsList").html()
+            res.send(content);
         } else {
             console.log(error)
         }
     })
 
-    res.send("Thank you");
 
 });
 
