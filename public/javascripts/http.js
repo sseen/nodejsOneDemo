0cfd6720ec071aa0f82c0ae2c5f9a2082eaec7fa
sseen <ssnfee@gmail.com>
2017-03-02 17:22:22 +0800

    add div to warp the existed item

--- public/javascripts/http.js
+++ public/javascripts/http.js
@@ -10,5 +10,15 @@ function getUrlContent() {
         $('#content').addClass('mui-content mui-scroll-wrapper')
         $('#content > ul').addClass('mui-table-view mui-table-view-chevron');
         $('#content > ul > li').addClass('mui-table-view-cell');
+        $('#content > ul > li a').addClass('mui-navigate-right');
+
+        $('#content > ul > li a').each(function () {
+            var newDiv = $('<div/>').addClass('mui-table-cell mui-col-xs-10');
+            $(this).before(newDiv);
+            var next = $(this).next();
+            newDiv.append(this).append(next);
+        })
+
+        $('#content > ul > li h4').addClass('mui-pull-right');
     });
 }
