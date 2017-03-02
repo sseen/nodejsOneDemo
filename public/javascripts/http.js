b0d8344f5f3b1fbfa7cf954de350c6c3e1af1bf2
sseen <ssnfee@gmail.com>
2017-03-02 18:02:44 +0800

    mui list accessoryType can modify ，table view cell padding and mui navigate right padding，and after:: padding

--- public/javascripts/http.js
+++ public/javascripts/http.js
@@ -12,13 +12,6 @@ function getUrlContent() {
         $('#content > ul > li').addClass('mui-table-view-cell');
         $('#content > ul > li a').addClass('mui-navigate-right');
 
-        $('#content > ul > li a').each(function () {
-            var newDiv = $('<div/>').addClass('mui-table-cell mui-col-xs-10');
-            $(this).before(newDiv);
-            var next = $(this).next();
-            newDiv.append(this).append(next);
-        })
-
         $('#content > ul > li h4').addClass('mui-pull-right');
     });
 }
